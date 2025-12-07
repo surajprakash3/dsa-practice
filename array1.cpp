bf4f2da86c67b569bece9fdb11945ae961fe6571
Suraj Prakash <surajprak201@gmail.com>
2025-12-07 19:39:07 +0530

    array1 commit

---
+++ array1.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+// int main(){
+    // array declare
+    // int arr[7];
+    // cout<<arr<<endl;
+    // cout<<&arr<<endl;
+
+    // cout<<"Aray create successfully"<<endl;
+
+    //  array inilitalise
+    //  int arr[]={2,3,4,6,7};
+    //  int brr[5]={2,3,5,6,6,};
+    //  int crr[10]={2,3,5,6,6};
+
+    //  error
+    // int drr[2]={3,4,5,};
+    //  cout<<"Array initalised successfully"<<endl;
+
+
+    // int arr[]={2,34,5,6,6};
+    // print all value114
+
+    // for(int index=0;index<5;index++  ){
+    //     cout<<arr[index]<<" ";
+    // }
+// int n;
+// cin>>n;
+// bad practice
+// int arr[n];
+
+// static array/
+// int arr[10000];
+
+
+//     int arr[10];
+//     cout<<"enter the input value in array"<<endl;
+//     for(int i=0;i<10;i++){
+//         cin>>arr[i];
+//     }
+//     // printing
+//     cout<<"printing the value or array"<<endl;
+//     for(int i=0;i<10;i++){
+//         cout<<arr[i]<<" ";
+         
+//     }
+//      return 0;
+// }
+
+
+// int main(){
+//     int arr[5000];
+//     int n;
+//     cout<<"How many numbers you want to add in array"<<endl;
+//     cin>>n;
+
+
+//     cout<<"Enter the numbers "<<endl;
+//     for(int i=0; i<n; i++ ){
+//         cin>>arr[i];
+
+//     }
+
+//     // print double
+//     cout<<"Double: ";
+//     for(int i=0; i<n;i++){
+//         cout<<2*arr[i]<<" ";
+//     }
+
+//     return 0;
+
+// }
+
+
+
+ 
